a2b810c9807868fe17a6525d52cde8f3ea63cd53
katarinnaq <kpetrovic2523it@raf.rs>
2026-05-21 14:13:35 +0200

    Manje izmene u izgledu aplikacije

---
+++ ProjekatSignalR/ProjekatSignalR/Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using ProjekatSignalR.Models;
+
+namespace ProjekatSignalR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // (npr. POST /api/Auth/register)
+    public class AuthController : ControllerBase
+    {
+        // UserManager sluzi za upravljanje korisnicima
+        private readonly UserManager<Korisnik> _userManager;
+        // SignInManager sluzi za login/logout funkcionalnosti
+        private readonly SignInManager<Korisnik> _signInManager;
+
+        // Konstruktor
+        public AuthController(UserManager<Korisnik> userManager, SignInManager<Korisnik> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // Registracija za novog korisnika
+        // POST /api/Auth/register
+        [HttpPost("register")]
+        public async Task <IActionResult> Register(RegisterModel model)
+        {
+            // Kreiranje novog korisnika na osnovu podataka iz modela
+            var user = new Korisnik
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            // Kreiranje korisnika u Identity sistemu sa zadatim passwordom
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            // Ako je kreiranje uspesno vracamo OK
+            if (result.Succeeded)
+            {
+                return Ok("Korisnik registrovan");
+            }
+
+            // Ako nije vracamo gresku
+            return BadRequest(result.Errors);
+        }
+
+        // Logovanje korisnika
+        // POST /api/Auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            // Provera podataka za login
+            var result = await _signInManager.PasswordSignInAsync(
+                    model.UserName, // ime
+                    model.Password, // sifra
+                    false,          // ne "remember me"
+                    false);         // ne blokiraj korisnika nakon neuspesnih pokusaja
+
+            // Ako je ulogovan uspesno vracamo ok
+            if (result.Succeeded)
+            {
+                return Ok("Uspesan login");
+            }
+
+            // Ako nije vracamo gresku
+            return BadRequest("Pogresni rezultati");
+        }
+    }
+
+    // Model za registraciju
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    // Model za login   
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ProjekatSignalR/ProjekatSignalR/Controllers/GroupController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjekatSignalR.Data;
+using ProjekatSignalR.Models;
+using System.Security.Claims;
+
+namespace ProjekatSignalR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // (npr. POST /api/Group/create)
+    public class GroupController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GroupController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Ucitavanje grupa
+        // GET /api/Group
+        [HttpGet]
+        public IActionResult GetGroups()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var groups = _context.ClanoviGrupe
+                .Where(x => x.KorisnikId == userId)
+                .Select(x => new { x.Grupa.Id, x.Grupa.Naziv })
+                .Distinct()
+                .ToList();
+
+            return Ok(groups);
+        }
+
+        // Ucitavanje istorije poruka
+        // GET /api/Group/messages/{groupId}
+        [HttpGet("messages/{groupId}")]
+        public IActionResult GetGroupMessages(int groupId)
+        {
+            var messages = _context.GrupnePoruke
+                .Where(m => m.GrupaId == groupId)
+                .Select(m => new { m.PosiljalacId, m.Poruka, m.DatumSlanja })
+                .ToList();
+
+            return Ok(messages);
+        }
+    }
+}
